17c50cdff09d79eb893489257dadff6c77a2156b
VulcanoAhab <vulcano.ahab@gmail.com>
2018-10-01 16:50:10 -0300

    Building module

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="pyAlexaTopSites",
+    version='0.1.0',
+    author="VulcanoAhab",
+    packages=["pyAlexaTopSites"],
+    url="https://github.com/VulcanoAhab/pyAlexaTopSites.git",
+    description="Alexa TopSites by Country",
+)
